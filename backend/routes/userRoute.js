93dd688a736b926264f99993f40f28c94dd0e85c
cappeloow <cappe1996@hotmail.com>
2023-12-27 15:56:21 +0100

    feat(getAllUsers, getSpecificUsers): endpoitns and controllers

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import sayHello from '../controller/userController.js';
-import { getUserQRCode, updateUserDetails } from '../controller/userController.js';
+import { getUserQRCode, updateUserDetails, getAllUsers, getSpecificUser } from '../controller/userController.js';
 const userRouter = express.Router();
 
 userRouter.get('/hello', sayHello);
@@ -9,4 +9,11 @@ userRouter.get('/hello', sayHello);
 userRouter.get('/qrcode/:userId', getUserQRCode)
 
 userRouter.put('/update/:userId', updateUserDetails);
+
+userRouter.get('/:userId', getSpecificUser);
+userRouter.get('/all', getAllUsers);
+
+
+
+
 export default userRouter;
