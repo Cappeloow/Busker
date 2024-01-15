2417f71a1726555d4c31b10ea8943fd098985d0d
cappeloow <cappe1996@hotmail.com>
2024-01-15 19:35:10 +0100

    feat(QRcode background color)

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -1,19 +1,34 @@
 import QRCode from 'qrcode';
 import { createCanvas, loadImage } from 'canvas';
+
 export default async function generateQRCode(id) {
     const userURL = process.env.BUSKER_URL + id;
     const canvas = createCanvas(300, 300);
+
     try {
-        await QRCode.toCanvas(canvas, userURL, { errorCorrectionLevel: 'H' });
-        const icon = await loadImage('./icons/check.png');
-        const x = (canvas.width - icon.width) / 2
-        const y = (canvas.height - icon.height) / 2
+        const qrOptions = {
+            errorCorrectionLevel: 'H',
+            margin: 4, // Border size (adjust as needed)
+            color: {
+                dark: '#00CED1', // Dark color (Turquoise)
+                light: '#33FF57' // Light color (Green)
+            }
+        };
+
+        await QRCode.toCanvas(canvas, userURL, qrOptions);
+
+        const icon = await loadImage('./icons/music-player.png');
+        const x = (canvas.width - icon.width) / 2;
+        const y = (canvas.height - icon.height) / 2;
+
         const context = canvas.getContext('2d');
         context.drawImage(icon, x, y);
+
         const base64Image = canvas.toDataURL('image/png');
         return base64Image;
     } catch (error) {
         console.error('Error generating QR code', error);
         return null;
     }
-};
+}
+
