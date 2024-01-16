3c01a93ea25b1c0d9be29407a82b70a7f746cb07
cappeloow <cappe1996@hotmail.com>
2024-01-16 19:20:22 +0100

    QR (Frontend Handling as image): cannot seem to see another solution than do a GET in this case - So I had to do a use client w use effect and usestate

--- frontend/app/contact/page.tsx
+++ frontend/app/contact/page.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+'use client';
 
-type Props = {}
+import React, { useEffect, useState } from 'react';
+import { generateQRCode } from '../services/services';
 
-function page({}: Props) {
+const Page = () => {
+  const [qrCode, setQRCode] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchQRCode = async () => {
+      try {
+        const data = await generateQRCode("237ae2c7-8e56-4383-83ac-d8ac52c5be5e");
+        setQRCode(data); // assuming data is the base64-encoded image
+      } catch (error:any) {
+        console.error('Error fetching QR code:', error.message);
+      }
+    };
+
+    fetchQRCode();
+  }, []);
+
   return (
-    <div>page</div>
-  )
-}
+    <div>
+      {qrCode && (
+        <img
+          src={`${qrCode}`}
+          alt="QR Code"
+          style={{ maxWidth: '100%', maxHeight: '100%' }}
+        />
+      )}
+    </div>
+  );
+};
 
-export default page
+export default Page;
