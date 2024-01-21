6de27a8b378c8b614eafbd2c47b940ba390f48ea
cappeloow <cappe1996@hotmail.com>
2024-01-21 19:46:23 +0100

    style(login):added some styling

--- frontend/app/components/LoginForm.tsx
+++ frontend/app/components/LoginForm.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
 
+"use client";
+import { FcGoogle } from "react-icons/fc";
+import { BUSKER_BACKEND_URL } from "../services/services";
+import Link from "next/link";
 type Props = {}
 
 function LoginForm({}: Props) {
   return (
-    <div>LoginForm</div>
+    <div className="auth-container">
+    <h1>BUSKER</h1>
+    <p><i> REAL </i>AUDIENCE START WITH <b>YOU</b></p>
+    <button className="google-auth-btn" onClick={() => {
+      window.location.href = `${BUSKER_BACKEND_URL}/auth/google`
+    }}>
+     <FcGoogle />
+    </button>
+    </div>
   )
 }
 
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,7 @@
-"use client";
 import { FcGoogle } from "react-icons/fc";
 import { BUSKER_BACKEND_URL } from "./services/services";
 import Link from "next/link";
+import LoginForm from "./components/LoginForm";
 export default function Home() {
   return (
     <main className="home-main">
@@ -13,6 +13,7 @@ export default function Home() {
           <li>WHY BUSKER</li>
         </ul>
       </header>
+       <LoginForm/>
     </main>
   )
 }
