99943b61f5e21d023a9e38517ddacdde630880dd
cappeloow <cappe1996@hotmail.com>
2024-01-23 15:12:23 +0100

    feat(links): create links only on your page

--- frontend/_components/UserProfile/CreateLinks.tsx
+++ frontend/_components/UserProfile/CreateLinks.tsx
@@ -1,18 +1,30 @@
 "use client"
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { createLink } from '@/app/services/services';
 import { useFormState } from 'react-dom';
+import { authStatus } from '@/app/services/services';
 import { useRouter, useParams } from 'next/navigation';
 type Props = {};
 
 export default function CreateLinks({}: Props) {
-    const router = useRouter();
-    const {id} = useParams();
-  const [linkData, setLinkData] = useState({
-    icon: '',
-    title: '',
-    url: '',
-  });
+  const router = useRouter();
+  const {id} = useParams();
+const [linkData, setLinkData] = useState({
+  icon: '',
+  title: '',
+  url: '',
+});
+const [isUser, setIsUser] = useState<boolean>(false)
+
+  useEffect(() => {
+    const getAuth = async () => {
+      const {userId} = await authStatus();
+      if (userId === id){
+        setIsUser(true);
+      }
+    }
+    getAuth();
+  },[])
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -22,6 +34,7 @@ export default function CreateLinks({}: Props) {
   };
 
   return (
+    isUser &&
     <div>
       <form onSubmit={handleSubmit}>
         <input
